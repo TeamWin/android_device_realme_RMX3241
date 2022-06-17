e424f4360e59b96c9e34a0fc1848b64202f06a87
Bharat Dev Burman <bharat.singh7924@gmail.com>
2022-06-17 22:54:14 +0530

    RMX3241: drop custom libinit in favour of OTA asserts
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>
    Change-Id: Id25afb460ecc18af4cc4b2eed04c150fc497919b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_DIR)/init
-
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_RMX3241.mk
 
 COMMON_LUNCH_CHOICES := twrp_RMX3241-eng
